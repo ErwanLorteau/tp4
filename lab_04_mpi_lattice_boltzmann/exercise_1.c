0789655d1e93bd0cefd52b09e1b7621bc83b6c9b
ErwanLorteau <87809072+ErwanLorteau@users.noreply.github.com>
2023-05-14 16:35:34 +0200

    initial commit 2

--- lab_04_mpi_lattice_boltzmann/exercise_1.c
+++ lab_04_mpi_lattice_boltzmann/exercise_1.c
@@ -25,43 +25,25 @@
 /****************************************************/
 void lbm_comm_init_ex1(lbm_comm_t * comm, int total_width, int total_height)
 {
-
-    int rank; // = mon id de noeud a moi quand je lance le code, commence a 0 et va a comm size -1
-
-    int comm_size;
-    MPI_Comm_rank( MPI_COMM_WORLD, &rank );
-    MPI_Comm_size( MPI_COMM_WORLD, &comm_size ); //comm_size replis auto avec le N passé en parametre (c'est le nombre de process)
-
-    //
+	//
 	// TODO: calculate the splitting parameters for the current task.
 	//
 	// HINT: You can look in exercise_0.c to get an example for the sequential case.
 	//
 
 	// TODO: calculate the number of tasks along X axis and Y axis.
-	comm->nb_x = total_width / comm_size ; // une seule ligne de domaine, chaque domaine de taille egale
-	comm->nb_y = 1 //une dimension en y ;
+	comm->nb_x = -1;
+	comm->nb_y = -1;
 
-    //chaque process doit avoir une part égale
-    if (total_width % comm->nb_x != 0 ) {
-        //abort
-    }
-
-
-    // TODO: calculate the current task position in the splitting
+	// TODO: calculate the current task position in the splitting
 	comm->rank_x = -1;
 	comm->rank_y = -1;
 
 	// TODO : calculate the local sub-domain size (do not forget the 
 	//        ghost cells). Use total_width & total_height as starting 
 	//        point.
-    if (//premier de la ligne) {
-        comm>width = (total_width / comm_size) + 1 ; // juste ue voisin a droite
-    else {
-        comm->width = (total_width / comm_size) + 2 ; //left and rigth neighboor
-    }
-
-	comm->height = total_height ; //tout le monde a la meme hauteur (c'est que coupé en ligne)
+	comm->width = -1;
+	comm->height = -1;
 
 	// TODO : calculate the absolute position in the global mesh.
 	//        without accounting the ghost cells
@@ -71,12 +53,6 @@ void lbm_comm_init_ex1(lbm_comm_t * comm, int total_width, int total_height)
 
 	//if debug print comm
 	//lbm_comm_print(comm);
-
-    /*
-#ifndef NDEBUG
-    lbm_comm_print( comm );
-#endif
-     */
 }
 
 /****************************************************/
